92276597b03f9102689bc1d6cb8ad9037006ef04
Aleksei <23leha77@mail.ru>
2022-01-07 17:17:13 +0300

    add_code

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
-let money=prompt('Введите бюджет на месяц'),
+let money=+prompt('Введите бюджет на месяц'),
 time=prompt('Введите дату в формате YYYY-MM-DD'), 
-cost=prompt('Во сколько обойдется?'),
-stat=prompt('Введите обязательную статью расходов в этом месяце'),
-expenses={
-[stat]:cost
-},
+
 appData={
-    'бюджет':money,
-    'данные времени':time,
-    expenses,
+    budget:money,
+    timeData:time,
+    expenses:{},
     optionalExpenses:{},
     income:{},
-    'savings':false
+    savings:false
 };
-alert(money);
+
+for (let i=0;i<2;i++){
+    let a=prompt('Введите обязательную статью расходов в этом месяце',''),
+    b=+prompt('Во сколько обойдется','');
+    if(typeof(a)==='string' && typeof(a)!=null && typeof(b)!=null && a!='' && b!='' && a.length<50){
+        appData.expenses[a]=b;
+    }
+   else{
+       i--;
+   }
+}
+appData.moneyPerday=appData.budget/30;
+if (appData.moneyPerday<100){
+    console.log('минимальный уровень достатка');
+}
+else
+    if (appData.moneyPerday>100 && appData.moneyPerday<2000){
+        
+        console.log('средний уровень достатка');
+
+    }
+else
+    if ( appData.moneyPerday>2000){
+        
+        console.log('высокий уровень достатка');
+
+    }
+
+
 console.log(appData);
