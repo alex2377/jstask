31d1dfaa39d77f4257a2b5c56678176b57b2ffc0
Aleksei <23leha77@mail.ru>
2022-01-08 11:46:37 +0300

    complete_task

--- index.js
+++ index.js
@@ -1,41 +1,79 @@
-let money=+prompt('Введите бюджет на месяц'),
-time=prompt('Введите дату в формате YYYY-MM-DD'), 
+let money,time;
+function start(){
+     money=+prompt('Введите бюджет на месяц');
+    time=prompt('Введите дату в формате YYYY-MM-DD');
+    while(isNaN(money) || money=="" || money==null){
+        money=+prompt('Введите бюджет на месяц');
+    }
+}
 
-appData={
+start();
+let appData={
     budget:money,
     timeData:time,
     expenses:{},
     optionalExpenses:{},
     income:{},
-    savings:false
+    savings:true
 };
 
-for (let i=0;i<2;i++){
-    let a=prompt('Введите обязательную статью расходов в этом месяце',''),
-    b=+prompt('Во сколько обойдется','');
-    if(typeof(a)==='string' && typeof(a)!=null && typeof(b)!=null && a!='' && b!='' && a.length<50){
-        appData.expenses[a]=b;
+function chooseExpenses(){
+    for (let i=0;i<2;i++){
+        let a=prompt('Введите обязательную статью расходов в этом месяце',''),
+        b=+prompt('Во сколько обойдется','');
+        if(typeof(a)==='string' && typeof(a)!=null && typeof(b)!=null && a!='' && b!='' && a.length<50){
+            appData.expenses[a]=b;
+        }
+       else{
+           i--;
+       }
     }
-   else{
-       i--;
-   }
 }
-appData.moneyPerday=appData.budget/30;
-if (appData.moneyPerday<100){
-    console.log('минимальный уровень достатка');
+chooseExpenses();
+function detectDayBudget(){
+    appData.moneyPerday=(appData.budget/30).toFixed();
+    alert("Ежедневный бюджет"+appData.moneyPerday);
 }
-else
-    if (appData.moneyPerday>100 && appData.moneyPerday<2000){
-        
-        console.log('средний уровень достатка');
-
+detectDayBudget();
+function detectLevel(){
+    if (appData.moneyPerday<100){
+        console.log('минимальный уровень достатка');
     }
-else
-    if ( appData.moneyPerday>2000){
-        
-        console.log('высокий уровень достатка');
-
+    else
+        if (appData.moneyPerday>100 && appData.moneyPerday<2000){
+            
+            console.log('средний уровень достатка');
+    
+        }
+    else
+        if ( appData.moneyPerday>2000){
+            
+            console.log('высокий уровень достатка');
+    
+        }
+    
+}
+detectLevel();
+function checkSavings(){
+    if(appData.savings==true){
+        let save=+prompt("Какова сумма накоплений",""),
+        percent=+prompt("Под какой процент","");
+        appData.monthIncome=save/100/12*percent;
+        alert("Доход в месяц с Вашего депозита:"+appData.monthIncome);
     }
 
-
+}
+checkSavings();
+function chooseOptExpenses(){
+    for (let i=0;i<3;i++){
+        let optExpenses=prompt("Статья необязательных расходов?","");
+        if(typeof(optExpenses)!=null && optExpenses!='' && optExpenses.length<50){
+            appData.optionalExpenses[i+1]=optExpenses;
+        }
+       else{
+        optExpenses=prompt("Статья необязательных расходов?","");
+       }
+    }
+}
+chooseOptExpenses();
 console.log(appData);
